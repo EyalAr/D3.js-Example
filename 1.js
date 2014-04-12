03ab9b3bd8b54e89ef7fa7745aa70b569e895d5f
Eyal Arubas <eyalarubas@gmail.com>
2014-04-12 23:02:23 +0800

    start 2nd example

---
+++ 1.js
@@ -0,0 +1,38 @@
+var data = [{
+    x: '25%',
+    y: '25%',
+    r: '5%',
+    c: 'red'
+}, {
+    x: '50%',
+    y: '50%',
+    r: '5%',
+    c: 'green'
+}, {
+    x: '75%',
+    y: '75%',
+    r: '5%',
+    c: 'blue'
+}];;
+
+var canvas = d3.select('#container');
+
+var circles = canvas
+    .selectAll('circle')
+    .data(data);
+
+circles
+    .enter()
+    .append('circle')
+    .attr('cx', function(d) {
+        return d.x;
+    })
+    .attr('cy', function(d) {
+        return d.y;
+    })
+    .attr('fill', function(d){
+        return d.c;
+    })
+    .attr('r', function(d) {
+        return d.r;
+    });
---
+++ 2.js
@@ -0,0 +1,48 @@
+var data = [{
+    x: '25%',
+    y: '25%',
+    r: '5%',
+    c: 'red'
+}, {
+    x: '50%',
+    y: '50%',
+    r: '5%',
+    c: 'green'
+}, {
+    x: '75%',
+    y: '75%',
+    r: '5%',
+    c: 'blue'
+}];;
+
+var canvas = d3.select('#container');
+
+function draw() {
+
+    var circles = canvas
+        .selectAll('circle')
+        .data(data);
+
+    circles
+        .enter()
+        .append('circle')
+        .attr('cx', function(d) {
+            return d.x;
+        })
+        .attr('cy', function(d) {
+            return d.y;
+        })
+        .attr('fill', function(d) {
+            return d.c;
+        })
+        .attr('r', 0)
+        .transition()
+        .ease('elastic')
+        .duration(750)
+        .attr('r', function(d) {
+            return d.r;
+        });
+
+}
+
+$('#container').click(draw);
